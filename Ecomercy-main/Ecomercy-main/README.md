f88fb83135df93b3df123348f6bddaf8667d1394
goncalohenrique <gonriq01@gmail.com>
2025-10-03 21:17:18 -0300

    commitando mudanças, arumando arquivos e diretoros.

--- Ecomercy-main/Ecomercy-main/README.md
+++
@@ -1,2 +0,0 @@
-# Ecomercy
-A e comerce
--- Ecomercy-main/Ecomercy-main/ecomercy/js/home.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    if(localStorage.getItem("sessao")){
-        document.getElementById("retorno").innerHTML = JSON.parse(localStorage.getItem("sessao")).email
-    }else{
-        window.location.href = "../login.html";
-    }
-});
