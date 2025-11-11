6e26a7b2bf2b1b682a49116001a343bbf19d4607
h-mello1008 <henriquemello1008@gmail.com>
2025-11-10 22:44:50 -0300

    sos

--- ecomercy/js/home.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    valida_sessao();
-    if(localStorage.getItem("sessao")){
-        document.getElementById("retorno").innerHTML = JSON.parse(localStorage.getItem("sessao")).email
-    }else{
-        window.location.href = "../login.html";
-    }
-});
--- ecomercy/php/conexao.php
+++ ecomercy/php/conexao.php
@@ -2,13 +2,10 @@
 // Variáveis de conexão com o Banco de Dados
 $servidor = "localhost:3306";
 $usuario  = "root";
-$senha    = "";
+$senha    = "Hm10082007$";
 $nome_banco = "Ecomercy";
 
 $conexao = new mysqli($servidor, $usuario, $senha, $nome_banco);
 if($conexao->connect_error){
     echo $conexao->connect_error;
 }
-
-
-// criarMateriais.js - editarMateriais.js - materiais.js - material_novo.php - material_alterar.php - material_excluir.php - criarMaterial.html - editarMaterial.html - materiais.html
